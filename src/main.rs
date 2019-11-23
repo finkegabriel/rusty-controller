5765f6b1bd91b7976af817d08a0030750f22cd40
Gabe <finkegabriel@gmail.com>
2019-11-23 11:49:50 -0700

    init

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+// jstest.rs
+use stick::Port;
+
+fn main() {
+    // Connect to all devices.
+    let mut port = Port::new();
+
+    // Loop showing state of all devices.
+    loop {
+        // Cycle through all currently plugged in devices.
+        let id = if let Some(a) = port.poll() {
+            a
+        } else {
+            continue;
+        };
+
+        if let Some(state) = port.get(id) {
+            println!("{}: {}", id, state);
+        }
+    }
+}
